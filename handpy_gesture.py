d094c0b86807fa32340b01f163a7186a319942ce
Fiddydoo <141464633+Fiddydoo1@users.noreply.github.com>
2025-01-28 17:55:29 +0100

    Create handpy_gesture.py

---
+++ handpy_gesture.py
@@ -0,0 +1,103 @@
+
+import time
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+import cv2 as cv
+import pylab as plt
+import threading
+
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+#this GestureRecognizerResult variable is different than handlandmarker
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+   hand_landmarks_list = detection_result.hand_landmarks
+   handedness_list = detection_result.handedness
+   annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected hands to visualize.
+   for idx in range(len(hand_landmarks_list)):
+     hand_landmarks = hand_landmarks_list[idx]
+     handedness = handedness_list[idx]
+
+    # Draw the hand landmarks.
+     hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+     hand_landmarks_proto.landmark.extend([
+       landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+     ])
+     solutions.drawing_utils.draw_landmarks(
+       annotated_image,
+       hand_landmarks_proto,
+       solutions.hands.HAND_CONNECTIONS,
+       solutions.drawing_styles.get_default_hand_landmarks_style(),
+       solutions.drawing_styles.get_default_hand_connections_style())
+
+    # Get the top left corner of the detected hand's bounding box.
+     height, width, _ = annotated_image.shape
+     x_coordinates = [landmark.x for landmark in hand_landmarks]
+     y_coordinates = [landmark.y for landmark in hand_landmarks]
+     text_x = int(min(x_coordinates) * width)
+     text_y = int(min(y_coordinates) * height) - MARGIN
+
+    # Draw handedness (left or right hand) on the image.
+     cv.putText(annotated_image, f"{handedness[0].category_name}",
+                 (text_x, text_y), cv.FONT_HERSHEY_DUPLEX,
+                 FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv.LINE_AA)
+
+   return annotated_image
+
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+
+    annotated_image = draw_landmarks_on_image(output_image.numpy_view(), result)
+    
+    cv.imshow("that", annotated_image)
+        
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        exit()
+    
+        
+options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path='C:/Users/Kompyuter/Desktop/newhand/hand_landmarker.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result
+    )
+
+gesture_recognizer = GestureRecognizer.create_from_options(options)
+
+cap = cv.VideoCapture(4)
+
+timestamp_ms = 0
+    
+if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+    timestamp_ms += 1
+
+    gesture_recognizer.detect_async(mp_image, timestamp_ms)
+cap.release()
+cv.destroyAllWindows
+   
+    
